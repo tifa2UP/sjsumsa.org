352d5dab600c2ff50c5deacf947c67e9442d4d0b
tifa2UP <tifa3333@gmail.com>
2017-01-07 16:44:00 +0200

    Add all menu items

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -5,7 +5,25 @@ import FlatButton from "material-ui/FlatButton";
 
 var NavBar = React.createClass({
     render: function () {
-        return(<AppBar title="SJSU MSA" className="navbar" iconElementRight={<FlatButton label="Menu items"/>}/>);
+        const menuItem = {
+            backgroundColor: 'transparent',
+            color: 'white'
+        };
+
+        const menuItems = {
+            margin: '0px',
+        };
+
+        const items = (
+            <div style={menuItems}>
+                <FlatButton label="Home" style={menuItem}/>
+                <FlatButton label="Events" style={menuItem}/>
+                <FlatButton label="About" style={menuItem}/>
+                <FlatButton label="Resources" style={menuItem}/>
+                <FlatButton label="Contact" style={menuItem}/>
+            </div>
+        );
+        return(<AppBar title="SJSU MSA" className="navbar" iconElementRight={items}/>);
     }
 });
 
