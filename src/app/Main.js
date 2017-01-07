702f27ca67228bbded75ae59958e8dd1659199c0
tifa2UP <tifa3333@gmail.com>
2017-01-07 11:20:24 +0200

    initial commit

--- src/app/Main.js
+++ src/app/Main.js
@@ -5,7 +5,7 @@
 import React, {Component} from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import Dialog from 'material-ui/Dialog';
-import {deepOrange500} from 'material-ui/styles/colors';
+import {cyan500} from 'material-ui/styles/colors';
 import FlatButton from 'material-ui/FlatButton';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -19,7 +19,7 @@ const styles = {
 
 const muiTheme = getMuiTheme({
   palette: {
-    accent1Color: deepOrange500,
+    accent1Color: cyan500,
   },
 });
 
@@ -36,13 +36,13 @@ class Main extends Component {
     this.setState({
       open: false,
     });
-  }
+  };
 
   handleTouchTap = () => {
     this.setState({
       open: true,
     });
-  }
+  };
 
   render() {
     const standardActions = (
--- webpack-production.config.js
+++ webpack-production.config.js
@@ -1,4 +1,4 @@
-const webpack = require('webpack');
+  const webpack = require('webpack');
 const path = require('path');
 const buildPath = path.resolve(__dirname, 'build');
 const nodeModulesPath = path.resolve(__dirname, 'node_modules');
