31b589466b732a00b0b1dbd36846ca906b8d81a5
tifa2UP <tifa3333@gmail.com>
2017-01-14 18:23:59 +0100

    make resources responsive

--- src/app/Resources.js
+++ src/app/Resources.js
@@ -5,6 +5,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 import Paper from 'material-ui/Paper'
 import RaisedButton from 'material-ui/RaisedButton'
+import FlatButton from 'material-ui/FlatButton'
 import {cyan300} from 'material-ui/styles/colors';
 
 var Resources = React.createClass({
@@ -20,12 +21,13 @@ var Resources = React.createClass({
             <div>
                 <Paper className="section no-padding" style={bgColor} rounded={false}>
                     <div id="praying-spacing">
-                        <div className="width60 alignTop">
+                        <div className="width60 alignTop section">
                             <h2>Praying Space</h2>
-                            <p className="section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mattis cursus libero pulvinar gravida.
+                            <p className="section-text resource-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mattis cursus libero pulvinar gravida.
                                 Sed purus risus,tempus a condimentum quis, tempor eget leo. Sed consectetur accumsan nulla sit amet
                                 laoreet.
                             </p>
+                            <FlatButton label="Location by video"/>
                         </div>
                         <div className="width40 ">
                             <img src="img/praying-space.jpg" className="block-img full-width" />
@@ -37,12 +39,13 @@ var Resources = React.createClass({
                         <div className="width40 ">
                             <img src="img/praying-space.jpg" className="block-img full-width" />
                         </div>
-                        <div className="width60 alignTop">
-                            <h2>Study Groups</h2>
-                            <p className="section-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mattis cursus libero pulvinar gravida.
+                        <div className="width60 alignTop section">
+                            <h2 className="">Study Groups</h2>
+                            <p className="section-text resource-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mattis cursus libero pulvinar gravida.
                                 Sed purus risus,tempus a condimentum quis, tempor eget leo. Sed consectetur accumsan nulla sit amet
                                 laoreet.
                             </p>
+                            <FlatButton label="Sign up"/>
                         </div>
                     </div>
                 </Paper>
