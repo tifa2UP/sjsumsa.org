f25336187ae7ad911b6e95a45f99b0b0d3fa0aa2
tifa2UP <tifa3333@gmail.com>
2017-01-21 15:03:36 -0800

    remove the contact form Fix navbar CSS on smaller devices

--- src/app/Contact.js
+++ src/app/Contact.js
@@ -7,21 +7,17 @@ import FlatButton from "material-ui/FlatButton";
 import {cyan300} from 'material-ui/styles/colors';
 
 const bgColor = {
-    backgroundImage:  'url("img/circles-25.jpg")',
+    backgroundColor: 'rgb(252, 252, 252)',
 };
 
 const leftAlign = {
     textAlign: 'left',
-    // color: 'white'
 };
 
 const fullWidth = {
     width: '100%'
 };
 
-const whiteColor = {
-    // color: 'white'
-};
 
 const buttonStyle = {
     marginTop: '20px',
@@ -36,58 +32,70 @@ export default class Contact extends React.Component {
     state = {
         name: '',
         category: null,
+        email: '',
+        message: '',
     };
 
-    // handleChange = (event, index, value) => this.setState({category});
+    handleNameChange(event) {
+        this.setState({
+
+        });
+        console.log(this.state.name);
+    };
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(this);
+    };
+    handleChange = (event, index, value) => this.setState({value});
 
     render () {
 
         return (
             <Paper className="section" style={bgColor} rounded={false} id="contact">
-                <h1 className="headingStyle1 ">CONTACT</h1>
+                <h2 className="headingStyle1 ">CONTACT</h2>
                 <div>
                     <div id="contact-form" className="alignTop">
-                        <form>
-                            <TextField
-                                type="text"
-                                fullWidth={true}
-                                floatingLabelText="Name"
-                                style={whiteColor}
-                            /><br />
-                            <SelectField style={leftAlign} fullWidth={true}
-                                         floatingLabelText="Category"
-                                         value={this.state.category}
-                                         onChange={this.handleChange}
-                                         menuItemStyle={blackColor}
-                            >
-                                <MenuItem value={1} primaryText="Suggestion" />
-                                <MenuItem value={2} primaryText="Event invitation" />
-                                <MenuItem value={3} primaryText="Question" />
-                                <MenuItem value={4} primaryText="Housing" />
-                                <MenuItem value={5} primaryText="Donation" />
-                                <MenuItem value={6} primaryText="Website bug/typo" />
-                                <MenuItem value={7} primaryText="Other" />
-                            </SelectField>
-                            <br />
+                        {/*<form onSubmit={this.handleSubmit} action="https://formspree.io/tifa3333@gmail.com" method="POST">*/}
+                            {/*<TextField*/}
+                                {/*type="text"*/}
+                                {/*fullWidth={true}*/}
+                                {/*floatingLabelText="Name"*/}
+                            {/*/><br />*/}
+                            {/*<SelectField style={leftAlign} fullWidth={true}*/}
+                                         {/*floatingLabelText="Category"*/}
+                                         {/*value={this.state.category}*/}
+                                         {/*onChange={this.handleChange}*/}
+                            {/*>*/}
+                                {/*<MenuItem value={1} primaryText="Suggestion" />*/}
+                                {/*<MenuItem value={2} primaryText="Event invitation" />*/}
+                                {/*<MenuItem value={3} primaryText="Question" />*/}
+                                {/*<MenuItem value={4} primaryText="Housing" />*/}
+                                {/*<MenuItem value={5} primaryText="Donation" />*/}
+                                {/*<MenuItem value={6} primaryText="Website bug/typo" />*/}
+                                {/*<MenuItem value={7} primaryText="Other" />*/}
+                            {/*</SelectField>*/}
+                            {/*<br />*/}
 
-                            <TextField fullWidth={true}
-                                       type="email"
-                                       floatingLabelText="E-mail"
-                                        style={whiteColor}
-                            /><br />
-                            <TextField style={leftAlign}  fullWidth={true}
-                                       multiLine={true}
-                                       rows={4}
-                                       rowsMax={4}
-                                       required="true"
-                                       floatingLabelText="Message"
-                            /><br />
-                        </form>
-                        <FlatButton label="Submit" style={buttonStyle} />
+                            {/*<TextField fullWidth={true}*/}
+                                       {/*type="email"*/}
+                                       {/*floatingLabelText="E-mail"*/}
+                            {/*/><br />*/}
+                            {/*<TextField style={leftAlign}  fullWidth={true}*/}
+                                       {/*multiLine={true}*/}
+                                       {/*rows={4}*/}
+                                       {/*rowsMax={4}*/}
+                                       {/*required="true"*/}
+                                       {/*floatingLabelText="Message"*/}
+                            {/*/><br />*/}
+                            {/*<FlatButton label="Submit" value="Submit" type="submit" style={buttonStyle} />*/}
+                        {/*</form>*/}
+                    E-mail: hi@sjsumsa.org
                     </div>
 
                     <div id="social" className="alignTop alignLeft">
-                        <h4 className="">Find us on social media</h4>
+                        <h4 className=""></h4>
+
                         <a href="https://www.facebook.com/groups/5696549276/" target="_blank"><i className="fa fa-facebook hvr-grow"></i></a>
                         <a href="https://www.instagram.com/msa_sjsu/" target="_blank"><i className="fa fa-instagram hvr-grow"></i> </a>
                     </div>
