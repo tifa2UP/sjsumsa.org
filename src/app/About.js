eeeae41c9b957924a13d60507dee32ddd7cda73d
tifa2UP <tifa3333@gmail.com>
2017-02-10 14:23:24 -0800

    add subscribtion button in the about section

--- src/app/About.js
+++ src/app/About.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Paper from 'material-ui/Paper';
-import {cyan500} from 'material-ui/styles/colors';
+import FlatButton from 'material-ui/FlatButton';
 var firebase = require('firebase');
 
 export default class About extends React.Component {
@@ -31,6 +31,7 @@ export default class About extends React.Component {
                 <h1 className="headingStyle1">ABOUT</h1>
                 <p className="section-text">The Muslim Student Association of San Jose State University is dedicated to building an awesome environment of brotherhood and sisterhood on campus through social, religious, interfaith, educational and outreach events. </p>
                 <p className="section-text">Our goal is to promote the true, balanced message of Islam within ourselves and to the general public by being the best possible Muslims we can be in terms of our manners, morale characters, and worship. We hope to achieve this inshallah through following the Qur’an and example of the Prophet Muhammad (peace be upon him).</p>
+                <FlatButton label="Subscribe to our mailing list"/>
             </Paper>
         );
     }
