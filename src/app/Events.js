eb27a514a22fae3d09b1927d0feed37282a875bb
tifa2UP <tifa3333@gmail.com>
2017-02-09 18:50:47 -0800

    Added ability to view archived events

--- src/app/Events.js
+++ src/app/Events.js
@@ -23,7 +23,14 @@ export default class Events extends React.Component{
 
     componentWillMount(){
         var database = firebase.database();
-        var eventsRef = database.ref().root.orderByChild("archived").equalTo(true);
+        var archivedEventsRef = database.ref().root.orderByChild("archived").equalTo(true);
+        var eventsRef = database.ref().root.orderByChild("archived").equalTo(false);
+        archivedEventsRef.on('value', snap => {
+            var archivedEvents = Object.values(snap.val());
+            this.setState({
+                archivedEvents: archivedEvents,
+            })
+        })
         eventsRef.on('value', snap => {
             var events = Object.values(snap.val());
             this.setState({
@@ -37,9 +44,16 @@ export default class Events extends React.Component{
 
         this.state = {
             events: [],
-            archivedEvent: [],
-            expanded: false,
+            archivedEvents: [],
+            displayArchive: false,
         }
+    }
+
+    viewArchivedEvents() {
+        // console.log("button clocked!")
+        this.state.displayArchive = true;
+        console.log(this.state.displayArchive)
+        this.forceUpdate();
     }
 
     render() {
@@ -49,16 +63,25 @@ export default class Events extends React.Component{
         };
 
 
-        var archivedEvents = (
-        <FlatButton label="Events Archive" style={archiveStyle}/>
-        );
+        var archivedEvents;
 
         const cardTextExpanded = false;
 
         var events = this.state.events.map(event =>
-        <EventCard key={Events.getKey()} title={event.title} type={event.type} pictureURL={event.pictureURL} description={event.description}
-                   dayAndTime={event.dayAndTime} locationAndDate={event.locationAndDate} RSVP={event.RSVP}></EventCard>
+            <EventCard key={Events.getKey()} title={event.title} type={event.type} pictureURL={event.pictureURL} description={event.description}
+                       dayAndTime={event.dayAndTime} locationAndDate={event.locationAndDate} RSVP={event.RSVP}></EventCard>
         );
+        console.log(this.state.displayArchive)
+
+        if (this.state.displayArchive){
+            archivedEvents =  this.state.archivedEvents.map(event =>
+                <EventCard key={Events.getKey()} title={event.title} type={event.type} pictureURL={event.pictureURL} description={event.description}
+                           dayAndTime={event.dayAndTime} locationAndDate={event.locationAndDate} RSVP={event.RSVP}></EventCard>
+            );
+        }
+        else{
+            archivedEvents = (<FlatButton label="Events Archive" style={archiveStyle} onTouchTap={() => this.viewArchivedEvents()}/>);
+        }
 
         return (
             <Paper className="section" rounded={false} zDepth={1} id="events">
@@ -66,7 +89,7 @@ export default class Events extends React.Component{
                 <div>
                     {events}
                 </div>
-                    {archivedEvents}
+                {archivedEvents}
             </Paper>
         );
     }
