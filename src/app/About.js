3e45a4e78d63297debff29a14e5ef7b61320416b
tifa2UP <tifa3333@gmail.com>
2017-02-10 18:21:56 -0800

    add link to mailing list. Add open graph image

--- src/app/About.js
+++ src/app/About.js
@@ -31,7 +31,7 @@ export default class About extends React.Component {
                 <h1 className="headingStyle1">ABOUT</h1>
                 <p className="section-text">The Muslim Student Association of San Jose State University is dedicated to building an awesome environment of brotherhood and sisterhood on campus through social, religious, interfaith, educational and outreach events. </p>
                 <p className="section-text">Our goal is to promote the true, balanced message of Islam within ourselves and to the general public by being the best possible Muslims we can be in terms of our manners, morale characters, and worship. We hope to achieve this inshallah through following the Qur’an and example of the Prophet Muhammad (peace be upon him).</p>
-                <FlatButton label="Subscribe to our mailing list"/>
+                <FlatButton label="Subscribe to our mailing list" href="https://docs.google.com/forms/d/e/1FAIpQLScIuuXRnuhuEWAw8uywj7aKyXyWd218fJKX1BLW0SJ6KP7Otg/viewform" target="_window"/>
             </Paper>
         );
     }
